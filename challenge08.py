7fd8a69d3c7398bb3d4242d2fb29c5b35911d769
julianp91 <juliancamilopenamanrique@gmail.com>
2024-04-04 00:37:54 -0500

    class08

---
+++ challenge08.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+my_list = ["Carrots", "Brussels sprouts", "Mushrooms", "Potatoes", "Bell peppers", "Green peas", "Beets", "Broccoli", "Cabbage", "Broccoflower"]
+
+print("fourth element of the list:", my_list[3])
+
+print("sixth through tenth elements of the list:", my_list[5:])
+
+my_list[6] = "onion"
+
+print("Updated list:", my_list)
