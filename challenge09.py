7d698df0275adb0e212cc7b763c2a8aacba829c0
julianp91 <juliancamilopenamanrique@gmail.com>
2024-04-04 20:19:44 -0500

    class09

---
+++ challenge09.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+
+
+# If statement using equality condition
+a = 1
+b = 4
+
+if a == b:
+    print("a is equal to b")
+elif a < b:
+    print("a is less than b")
+else:
+    print("a is greater than b")
+
+
+if a != b:
+    print("a is not equal to b")
+elif a < b:
+    print("a is less than b")
+else:
+    print("a is greater than b")
