0600911652e17433131b6f07145bd01423079d3f
julianp91 <juliancamilopenamanrique@gmail.com>
2024-04-08 22:04:35 -0500

    class11

---
+++ challenge11.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+#Script Name:          ops301-challenge11
+#author:               julian pena
+#date:                 04/08/2024
+
+
+import psutil
+
+
+print (psutil.cpu_times())
